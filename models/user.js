dd11e827d754298296606011b7754fd6ed41ae04
aristrisnawan <aristrisnawan35@gmail.com>
2023-12-29 20:04:25 +0700

    add function for signup

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// Hash the password before saving to the database
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10)
+    }
+    next()
+})
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password)
+} 
+
+module.exports = mongoose.model('User', userSchema)
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 
 //SignUp
 router.post('/signup', async(req, res) => {
-    res.send('Sign Up')
+    try {
+      const {username,password} = req.body
+      const user = new User({username,password})
+      const data = await user.save() 
+      res.status(201).json({data: data , message: 'User registered successfully'}) 
+    } catch (error) {
+        res.status(500).json({error: 'Internal server Error'})
+    }
 })
 
 //Login
